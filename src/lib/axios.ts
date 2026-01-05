52d6a5954c620a4b292bb7c97d842b1de3d37526
safdar-azeem <safdarazeem00@gmail.com>
2026-01-05 22:55:44 +0500

    Add axios.ts

---
+++ src/lib/axios.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useAuthStore } from '@/store/useAuthStore'
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4003/api'
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().logout()
+    }
+    return Promise.reject(error)
+  }
+)
